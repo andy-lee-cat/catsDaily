0ce96599ae31482067db12374ddf7453d2945f9b
andy-lee-cat <2048513251@qq.com>
2024-12-21 22:04:06 +0800

    star math

---
+++ 241221.cpp
@@ -0,0 +1,32 @@
+// 1285c 1400
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+void solve() {
+    long long x;
+    cin >> x;
+    long long resa = x, resb = x;
+    for (int i = 1; i <= 1e6; i++) {
+        if (x % i == 0) {
+            long long a = i, b = x / i;
+            if (__gcd(a, b) == 1) {
+                if (max(a, b) < max(resa, resb)) {
+                    resa = a;
+                    resb = b;
+                }
+            }
+        }
+    }
+    cout << resa << ' ' << resb << '\n';
+}
+
+int main() {
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
