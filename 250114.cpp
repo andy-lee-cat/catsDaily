91b27cef5d24f8c356f30cacc7090641c28cdbf4
andy-lee-cat <2048513251@qq.com>
2025-01-14 13:45:57 +0800

    star math

---
+++ 250114.cpp
@@ -0,0 +1,42 @@
+// 1514C 1600
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> res;
+    int mul = 1;
+    for (int i = 1; i < n; i++) {
+        if (__gcd(i, n) != 1) continue;
+        res.push_back(i);
+        mul = (mul * i) % n;
+        if (__gcd(mul, n) != 1) {
+            cerr << "Error: " << i << ' ' << mul << '\n';
+        }
+    }
+    if (mul == 1) {
+        cout << res.size() << '\n';
+        for (int i = 0; i < res.size(); i++) {
+            cout << res[i] << " \n"[i == res.size() - 1];
+        }
+    } else {
+        cout << res.size() - 1 << '\n';
+        for (int i = 0; i < res.size(); i++) {
+            if (res[i] == mul) continue;
+            cout << res[i] << ' ';
+        }
+        cout << '\n';
+    }
+}
+
+int main() {
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
