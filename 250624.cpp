b7dae27d1edea72b914a53a8586163cc327dbf49
andy-lee-cat <2048513251@qq.com>
2025-06-24 19:36:53 +0800

    star implementation

--- 250624.cpp
+++ 250624.cpp
@@ -13,6 +13,28 @@ struct Node {
         : idxl(idxl), idxr(idxr), l(l), r(r) {}
 };
 
+void solve2() {
+    string s;
+    long long pos;
+    cin >> s >> pos;
+    pos--;
+    vector<char> stk;
+    int curlen = s.length();
+    s += char('a' - 1);
+    bool ok = pos < curlen;
+    for (char c : s) {
+        while (!ok && !stk.empty() && stk.back() > c) {
+            stk.pop_back();
+            pos -= curlen;
+            curlen--;
+            if (pos < curlen)
+                ok = true;
+        }
+        stk.push_back(c);
+    } 
+    cout << stk[pos];
+}
+
 void solve() {
     // case1: s[0] > s[1]: remove s[0]
     // case2: s[0] <= s[1] <= s[2] ... > s[k]: remove s[k-1]
@@ -66,7 +88,7 @@ int main() {
     int t = 1;
     cin >> t;
     while (t--) {
-        solve();
+        solve2();
     }
     cout << '\n';
     return 0;
